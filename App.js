321b3fc213c58d488be319d890a99e74a71e7436
bsorawit123 <bsorawit123@gmail.com>
2023-01-01 14:26:56 +0700

    First Todo

--- App.js
+++ App.js
@@ -1,11 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const [editMode, setEditMode] = useState();
+  const [editInput, setEditInput] = useState("");
+
+  const addTodo = () => {
+    setTodos((currentTodo) => [
+      ...currentTodo,
+      { id: Math.random(), text: input },
+    ]);
+    setInput("");
+  };
+
+  const editTodo = (id) => {
+    const newTodos = todos.filter((todo) => {
+      if (todo.id == id) todo.text = editInput;
+      return todo;
+    });
+
+    setTodos(newTodos);
+    setEditMode(null);
+  };
+
+  const deleteTodo = (id) => {
+    const newTodo = todos.filter((todo) => todo.id != id);
+    setTodos(newTodo);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your todo..."
+          value={input}
+          onChangeText={(e) => setInput(e)}
+        />
+        <Button title="ADD" onPress={addTodo} />
+      </View>
+      <View style={styles.listContainer}>
+        {todos.map((todo) => (
+          <View style={styles.listItem} key={todo.id}>
+            {editMode == todo.id ? (
+              <View>
+                <TextInput
+                  value={editInput}
+                  onChangeText={(e) => setEditInput(e)}
+                />
+                <Button title="EDIT" onPress={() => editTodo(todo.id)} />
+              </View>
+            ) : (
+              <View>
+                <Text style={styles.listItemText}>{todo.text}</Text>
+                <View style={styles.listItemButton}>
+                  <Button
+                    onPress={() => {
+                      setEditMode(todo.id);
+                      setEditInput(todo.text);
+                    }}
+                    title="Edit"
+                  />
+                  <Button onPress={() => deleteTodo(todo.id)} title="Delete" />
+                </View>
+              </View>
+            )}
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +77,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  listContainer: {
+    marginTop: 20,
+  },
+  listItem: {
+    marginVertical: 10,
+    padding: 10,
+
+    borderWidth: 2,
+    borderRadius: 6,
+    borderColor: "#6259ff",
+  },
+  listItemText: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  listItemButton: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginHorizontal: 10,
   },
 });
